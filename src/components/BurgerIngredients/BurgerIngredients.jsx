a901b25969d206851e163eeb3556a4ebebc15b07
Vladimir Razinsky <razinskiy@wildberries.ru>
2023-02-12 21:41:53 +0300

    part 2

--- src/components/BurgerIngredients/BurgerIngredients.jsx
+++ src/components/BurgerIngredients/BurgerIngredients.jsx
@@ -1,15 +1,16 @@
 import burgerIngredientsStyles from './BurgerIngredients.module.css'
 import { Tab, Counter, CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components'
-import { useState, useRef, useMemo } from 'react'
+import { useState, useRef, useMemo, useEffect } from 'react'
 import IngredientDetails from '../IngredientDetails/IngredientDetails'
 import { useSelector } from 'react-redux';
+
+const SCROLL_DIST_BOUND = 41;
 
 function BurgerIngredients() {
     const [current, setCurrent] = useState('bun')
     const [isModalVisible, setIsModalVisible] = useState(false)
     const [ingredientDetails, setIngredientDetails] = useState(null)
-    const {ingredients, ingredientsFailed, ingredientsRequest} = useSelector(store => store.ingredientsReducer)
-    console.log({ingredients, ingredientsFailed, ingredientsRequest})
+    const {ingredients} = useSelector(store => store.ingredientsReducer)    
     const handleTabClick = (current) => {
         switch(current) {
             case 'bun':
@@ -46,13 +47,33 @@ function BurgerIngredients() {
     const bunRef = useRef(null)
     const sauceRef = useRef(null)
     const mainRef = useRef(null)
+    const tabRef = useRef(null)
+
+    const parentRect = useRef(null);
+    useEffect(() => {
+        parentRect.current = tabRef.current.getBoundingClientRect()
+    },[])
     
     const modalOptions = {isVisible: isModalVisible, onClose:onModalClose, title: 'Детали ингредиента'}
+   
 
+    const handleScroll = (e) => {                
+        const sauceDist = sauceRef.current.getBoundingClientRect().top - parentRect.current.bottom;
+        const mainDist =  mainRef.current.getBoundingClientRect().top - parentRect.current.bottom;        
+        if (mainDist <= SCROLL_DIST_BOUND) {    
+            setCurrent('main')            
+        }
+        else if (sauceDist <= SCROLL_DIST_BOUND) {
+            setCurrent('sauce')            
+        }
+        else {
+            setCurrent('bun')
+        }
+    }
 
     return (        
         <div>       
-        <div style={{ display: 'flex' }} className='mb-10'>
+        <div style={{ display: 'flex' }} className='mb-10' ref={tabRef}>
             <Tab value="bun" active={current === 'bun'} onClick={handleTabClick}>
                 Булки
             </Tab>
@@ -63,7 +84,7 @@ function BurgerIngredients() {
                 Начинки
             </Tab>
         </div>
-        <div className={burgerIngredientsStyles.container}>
+        <div className={burgerIngredientsStyles.container} onScroll={handleScroll}>
             <div className='text text_type_main-large mb-6' ref={bunRef}>
                 Булки
             </div>
